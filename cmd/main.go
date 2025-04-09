7196c52ccb9fbcc4401c0fab7152772fc763dc9f
iyasz <zakamaragames@gmail.com>
2025-04-09 22:24:18 +0700

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+
+	
+	http.ListenAndServe(":3000", r)
+}
